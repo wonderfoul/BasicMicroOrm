bd0b15646a009fe42c5493affd938415e3ed9294
wonderfoul <katten111>
2014-02-16 10:28:35 +0100

    Initial commit

---
+++ DatabaseAttributes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicMicroOrm
+{
+    /// <summary>   Attribute to identify if the column is a primary key. </summary>
+    ///
+    /// <remarks>   Nsl, 08.01.2013. </remarks>
+
+    public class PrimaryKey : Attribute
+    {
+    }
+
+    /// <summary>   Attribute to identify if the column is identity. </summary>
+    ///
+    /// <remarks>   Nsl, 08.01.2013. </remarks>
+
+    public class Identity : Attribute
+    {
+    }
+
+    /// <summary>   Attibute for the mappers to Ignore mapping. </summary>
+    ///
+    /// <remarks>   Nsl, 26.02.2013. </remarks>
+
+    public class IgnoreMapping : Attribute
+    {
+    }
+}
---
+++ MultiQueryResultSet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicMicroOrm
+{
+    /// <summary>   Multi query result set. </summary>
+    ///
+    /// <remarks>   Nsl, 25.01.2013. </remarks>
+    ///
+    /// <typeparam name="T">    Generic type parameter. </typeparam>
+    /// <typeparam name="U">    Generic type parameter. </typeparam>
+    /// <typeparam name="V">    Generic type parameter. </typeparam>
+    /// <typeparam name="W">    Type of the w. </typeparam>
+
+    public class MultiQueryResultSet<T, U, V, W>
+    {
+
+        /// <summary>   Gets or sets the first. </summary>
+        ///
+        /// <value> The first. </value>
+
+        public IEnumerable<T> FirstResult { get; set; }
+
+        /// <summary>   Gets or sets the second result. </summary>
+        ///
+        /// <value> The second result. </value>
+
+        public IEnumerable<U> SecondResult { get; set; }
+
+        /// <summary>   Gets or sets the third result. </summary>
+        ///
+        /// <value> The third result. </value>
+
+        public IEnumerable<V> ThirdResult { get; set; }
+
+        /// <summary>   Gets or sets the fourth result. </summary>
+        ///
+        /// <value> The fourth result. </value>
+
+        public IEnumerable<W> FourthResult { get; set; }
+
+
+
+    }
+}
